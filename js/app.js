4e4b06993271872e0a651aa3ced9cb377043d319
Tomasz Olejniczak <t-olejniczak@wp.pl>
2018-11-23 16:40:39 +0100

    Task 10.2 completed on new branch

--- js/app.js
+++ js/app.js
@@ -1,20 +1,30 @@
+'use strict';
+(function(){
+
+  var templateItem = document.getElementById('template_item').innerHTML;
+
+  Mustache.parse(templateItem);
+
+  var listItems = '';
+
+  for(var i = 0; i < slideList.length; i++){
+    listItems += Mustache.render(templateItem, slideList[i]);
+  }
+
+  var fullSlidesList = Mustache.render(templateItem, {slides: listItems});
+
+  var results = document.querySelector('.main-carousel');
+  results.insertAdjacentHTML('beforeend', fullSlidesList);
+
+})();
+
+
 var elem = document.querySelector('.main-carousel');
 var flkty = new Flickity( elem, {
-  // options
   cellAlign: 'left',
   contain: true,
-  "pageDots": false,
-  "hash": true
+  hash: true
 });
-
-// element argument can be a selector string
-//   for an individual element
-var flkty = new Flickity( '.main-carousel', {
-  // options
-
-});
-
-var flkty = new Flickity('.main-carousel');
 
 var progressBar = document.querySelector('.progress-bar')
 
@@ -23,17 +33,15 @@ flkty.on( 'scroll', function( progress ) {
   progressBar.style.width = progress * 100 + '%';
 });
 
-var flkty = new Flickity('.main-carousel');
-
 var buttonGroup = document.querySelector('.button-group');
 var buttons = buttonGroup.querySelectorAll('.button');
-buttons = fizzyUIUtils.makeArray( buttons );
+buttons = fizzyUIUtils.makeArray(buttons);
 
-buttonGroup.addEventListener( 'click', function( event ) {
-  // filter for button clicks
-  if ( !matchesSelector( event.target, '.button' ) ) {
-    return;
-  }
-  var index = buttons.indexOf( event.target );
-  flkty.select( index );
+buttonGroup.addEventListener('click', function (event) {
+
+    if (!matchesSelector(event.target, '.button')) {
+        return;
+    }
+    var index = buttons.indexOf(event.target);
+    flkty.select(index);
 });
