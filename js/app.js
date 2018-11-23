3ce3bee0e0974c583607a2b68c0ea0cb2c6d5236
Tomasz Olejniczak <t-olejniczak@wp.pl>
2018-11-23 18:39:26 +0100

    Google maps added. Task 10.3 completed

--- js/app.js
+++ js/app.js
@@ -45,3 +45,22 @@ buttonGroup.addEventListener('click', function (event) {
     var index = buttons.indexOf(event.target);
     flkty.select(index);
 });
+
+//Google Maps
+
+window.initMap = function () {
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 34.052368, lng: -118.247412 },
+        zoom: 6
+    });
+
+    for (var i = 0; i < slideList.length; i++) {
+        var marker = new google.maps.Marker({
+            position: slideList[i].coords,
+            map: map
+        });
+    }
+}
+
+initMap();
