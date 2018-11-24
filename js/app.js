8e01aa7846d651c8abf4b552510198e17bfd2f91
Tomasz Olejniczak <t-olejniczak@wp.pl>
2018-11-24 20:25:05 +0100

    Some small changes in style.css and app.js

--- js/app.js
+++ js/app.js
@@ -1,39 +1,25 @@
-var elem = document.querySelector('.main-carousel');
-var flkty = new Flickity( elem, {
-  // options
-  cellAlign: 'left',
-  contain: true,
-  "pageDots": false,
-  "hash": true
+var flkty = new Flickity('.main-carousel', {
+    cellAlign: 'left',
+    contain: true,
+    hash: true,
 });
-
-// element argument can be a selector string
-//   for an individual element
-var flkty = new Flickity( '.main-carousel', {
-  // options
-
-});
-
-var flkty = new Flickity('.main-carousel');
-
-var progressBar = document.querySelector('.progress-bar')
-
-flkty.on( 'scroll', function( progress ) {
-  progress = Math.max( 0, Math.min( 1, progress ) );
-  progressBar.style.width = progress * 100 + '%';
-});
-
-var flkty = new Flickity('.main-carousel');
 
 var buttonGroup = document.querySelector('.button-group');
 var buttons = buttonGroup.querySelectorAll('.button');
-buttons = fizzyUIUtils.makeArray( buttons );
+buttons = fizzyUIUtils.makeArray(buttons);
 
-buttonGroup.addEventListener( 'click', function( event ) {
-  // filter for button clicks
-  if ( !matchesSelector( event.target, '.button' ) ) {
-    return;
-  }
-  var index = buttons.indexOf( event.target );
-  flkty.select( index );
+buttonGroup.addEventListener('click', function (event) {
+
+    if (!matchesSelector(event.target, '.button')) {
+        return;
+    }
+    var index = buttons.indexOf(event.target);
+    flkty.select(index);
+});
+
+var progressBar = document.querySelector('.progress-bar')
+
+flkty.on('scroll', function (progress) {
+    progress = Math.max(0, Math.min(1, progress));
+    progressBar.style.width = progress * 100 + '%';
 });
