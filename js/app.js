df48cc8cc2b4db254b2a767748c1716714cf950a
Tomasz Olejniczak <t-olejniczak@wp.pl>
2018-11-27 00:11:49 +0100

    Addition changes in index.html and app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
-'use strict';
-(function(){
+//Slides - generator
 
   var templateItem = document.getElementById('template_item').innerHTML;
 
@@ -11,13 +10,10 @@
     listItems += Mustache.render(templateItem, slideList[i]);
   }
 
-  var fullSlidesList = Mustache.render(templateItem, {slides: listItems});
-
   var results = document.querySelector('.main-carousel');
-  results.insertAdjacentHTML('beforeend', fullSlidesList);
+  results.insertAdjacentHTML('beforeend', listItems);
 
-})();
-
+//Options
 
 var elem = document.querySelector('.main-carousel');
 var flkty = new Flickity( elem, {
@@ -26,12 +22,16 @@ var flkty = new Flickity( elem, {
   hash: true
 });
 
+//Progress Bar
+
 var progressBar = document.querySelector('.progress-bar')
 
 flkty.on( 'scroll', function( progress ) {
   progress = Math.max( 0, Math.min( 1, progress ) );
   progressBar.style.width = progress * 100 + '%';
 });
+
+//Reset button
 
 var buttonGroup = document.querySelector('.button-group');
 var buttons = buttonGroup.querySelectorAll('.button');
